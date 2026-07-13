ddedab0f5a8ff517f7463f5cb1908dc271080853
Paloma Araujo <paloma.araujo94@oulook.com>
2026-07-13 14:51:17 -0300

    adiciona area de estudo

---
+++ app/area-estudos/page.tsx
@@ -0,0 +1,12 @@
+export default function AreaEstudos() {
+  return (
+    <iframe
+      src="/area-estudos/index.html"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "none",
+      }}
+    />
+  );
+}
